ccacd24d3d831abd1fafcf660dfce0a2c1405b75
jsta <jstachelek@utexas.edu>
2017-01-03 14:51:53 -0500

    add milakes dataset

---
+++ R/wikilake-package.R
@@ -0,0 +1,24 @@
+#' Michigan Lakes
+#'
+#' Metadata of Michigan lakes scraped from Wikipedia.
+#'
+#' @format A data frame with 48 columns and 177 rows:
+#' \itemize{
+#'           \item{Name: }{lake name}
+#'           \item{Location: }{location description}
+#'           \item{Primary inflows: }{rivers and streams}
+#'           \item{Basin countries: }{countries}
+#'           \item{Surface area: }{hectares}
+#'           \item{Max. depth: }{meters}
+#'           \item{Surface elevation: }{meters}
+#'           \item{Lat: }{decimal degrees}
+#'           \item{Lon: }{decimal degrees}
+#'           \item{Primary outflows: }{rivers and streams}
+#'           \item{Average depth: }{meters}
+#'           \item{Max. length: }{meters}
+#'           \item{Max. width: }{meters}
+#'           }
+#' @docType data
+#' @keywords datasets
+#' @name milakes
+NULL
